b69e34553776097d76af2b6c1cf82d4dc8928561
brandonbrombacher <bbrombacher@me.com>
2019-04-16 22:45:13 -0700

    first commit

---
+++ src/test/java/appium/demo4/AppiumTest.java
@@ -0,0 +1,55 @@
+package appium.demo4;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+
+public class AppiumTest {
+
+	public AppiumDriver driver;
+	
+	@Before
+    public void setUp() throws IOException {
+            
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+		//capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 120);
+		capabilities.setCapability("deviceName", "LG V20");
+		capabilities.setCapability("noReset", true);
+		capabilities.setCapability("automationName", "uiautomator2");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("platformVersion", "8.0");
+		capabilities.setCapability("appPackage", "io.appium.android.apis");
+		capabilities.setCapability("appActivity","io.appium.android.apis.ApiDemos");
+		driver = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+
+    }
+    
+
+    public void tearDown() {
+        driver.quit();
+    }
+    
+    @Test
+    public void testOpensAlert() {
+
+    	System.out.println("testOpensAlert()");
+    }
+	
+}
